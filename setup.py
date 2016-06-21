64c88221e72a7ef372210267a4c35b2f2cee4c14
Steven Arcangeli <stevearc@stevearc.com>
2016-06-20 21:12:34 -0700

    Run tests on python 3.5

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
         ],
         author='Steven Arcangeli',
         author_email='stevearc@stevearc.com',
