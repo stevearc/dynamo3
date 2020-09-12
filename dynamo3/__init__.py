71f109bd8089633594c908e153b8dae89a446bd8
Steven Arcangeli <stevearc@stevearc.com>
2020-09-12 15:47:39 -0700

    Enable mypy no-implicit-reexport

--- dynamo3/__init__.py
+++ dynamo3/__init__.py
@@ -12,4 +12,25 @@ from .rate import RateLimit
 from .result import Capacity, Limit
 from .types import Binary, Dynamizer, is_null
 
+__all__ = [
+    "DynamoDBConnection",
+    "CheckFailed",
+    "ConditionalCheckFailedException",
+    "DynamoDBError",
+    "ProvisionedThroughputExceededException",
+    "ThroughputException",
+    "DynamoKey",
+    "GlobalIndex",
+    "IndexUpdate",
+    "LocalIndex",
+    "Table",
+    "Throughput",
+    "RateLimit",
+    "Capacity",
+    "Limit",
+    "Binary",
+    "Dynamizer",
+    "is_null",
+]
+
 __version__ = "1.0.0.dev0"
